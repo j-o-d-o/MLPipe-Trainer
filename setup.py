8d8146fea57fdf45f28f9303330a92649b3d4937
Jo <jdobler@protonmail.com>
2019-07-21 16:08:08 +0200

    release commit v0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mlpipe-trainer',
-    version='0.3.0',
+    version='0.3.1',
     author="Johannes Dobler",
     author_email="jdobler@protonmail.com",
     description="Manage training results, weights and data flow of your Tensorflow models",
